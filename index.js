4192f33aae405cf692c0b7dbdbaaeaf36927a2cb
Kashinka <azalena@yandex.ru>
2023-04-01 22:31:20 +0300

    Solved 1st task

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Найди карточку Стёпы в коде и запиши пол Стёпы в переменную 
 // Измени пол Стёпы на мужской используя метод textContent
+
+let card__info = document.querySelector('.catalog');
+
+let card__text = card__info.querySelector('.card__text');
+
+let styopaGender = card__text.querySelector('.gender').textContent;
+
+console.log(styopaGender);
