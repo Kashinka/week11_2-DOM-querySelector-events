847cccd8d02aa2cfb670be627404cff8fec7b7b4
Kashinka <azalena@yandex.ru>
2023-04-02 20:01:04 +0300

    Solved task 2

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ let card__info = document.querySelector('.catalog');
 
 let card__text = card__info.querySelector('.card__text');
 
-let styopaGender = card__text.querySelector('.gender').textContent;
+let styopaGender = card__text.querySelector('.gender').textContent = "мальчик";
 
 console.log(styopaGender);
