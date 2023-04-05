48ec6079fd8f897fa1545a21d1e1af5d86d4b265
Kashinka <azalena@yandex.ru>
2023-04-05 15:02:04 +0300

    Added class white to html

--- index2.js
+++ index2.js
@@ -1,5 +1,6 @@
 // при клике по кнопке скрипт запустит код
 button.onclick = function () {
     let white = document.querySelector('.white');
+    console.log(white);
     white.classList.add('background-color: red'); // напишите условие при котором будет переключаться класс white у кнопки (класс переключает цвет бэкграунда)
   }
