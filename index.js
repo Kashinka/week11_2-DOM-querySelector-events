7187bcb5dd18d925b849dc33411461a013884848
Kashinka <azalena@yandex.ru>
2023-04-21 16:30:32 +0300

    Added function

--- index.js
+++ index.js
@@ -11,4 +11,6 @@ console.log(styopaGender);
 
 // Добавь в карточки кнопку “Узнать больше”. При клике по кнопке, будет открываться страница Википедии с описанием породы котика.
 
-
+function openInfo () {
+    window.open(document.https:ru.wikipedia.org/wiki/%D0%91%D0%B5%D0%BD%D0%B3%D0%B0%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F_%D0%BA%D0%BE%D1%88%D0%BA%D0%B0_(%D0%B4%D0%BE%D0%BC%D0%B0%D1%88%D0%BD%D1%8F%D1%8F));
+};
