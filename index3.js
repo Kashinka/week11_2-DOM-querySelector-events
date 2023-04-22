ed8223bf9c5d94b9edd87fa1bff667d42e4d327e
Kashinka <azalena@yandex.ru>
2023-04-22 14:17:31 +0300

    Added function for 1st cross

--- index3.js
+++ index3.js
@@ -1,3 +1,11 @@
 // Есть список сообщений
 
 // При помощи JavaScript для каждого сообщения добавьте слушатель события на крестик (один элемент - один слушатель), который по нажатию удаляет <div>
+
+let cross1 = document.querySelector('#cross1');
+
+function delDiv() {
+    document.getElementById("div1").remove();
+}
+
+cross1.addEventListener('click', delDiv);
