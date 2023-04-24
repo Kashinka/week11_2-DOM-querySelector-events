bb25374a054ef340c3b893d17b0f9efc63c8f525
Kashinka <azalena@yandex.ru>
2023-04-24 13:51:06 +0300

    Solved task

--- index4.js
+++ index4.js
@@ -4,3 +4,8 @@
 // 2. Все элементы label внутри этой таблицы (их три)
 // 3. Форму form с именем `name="search-person"`
 
+let table = document.getElementById('age-table');
+
+let elems = table.getElementsByTagName('label');
+
+let form = document.getElementsByName('search-person');
