0b029086b33c30d853199658232a061f290601f5
Kashinka <azalena@yandex.ru>
2023-05-01 13:58:02 +0300

    Added innerHTMLs for other cost

--- basket.js
+++ basket.js
@@ -9,3 +9,15 @@ let sourcreamCost = 95;
 const result1 = document.getElementById("result1");
 
 result1.innerHTML = `Огурцы ${cucumberCost} руб.`;
+
+const result2 = document.getElementById("result2");
+
+result2.innerHTML = `Помидоры ${tomatoCost} руб.`;
+
+const result3 = document.getElementById("result3");
+
+result3.innerHTML = `Зелень ${greeneryCost} руб.`;
+
+const result4 = document.getElementById("result4");
+
+result4.innerHTML = `Сметана ${sourcreamCost} руб.`;
